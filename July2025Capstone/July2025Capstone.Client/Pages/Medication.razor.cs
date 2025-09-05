13f4459bf0564f27fcf89efc18f548fa1b6a4d77
Khan <978294@corp.trizetto.com>
2025-09-05 14:52:06 -0600

    Remove commented code

--- July2025Capstone/July2025Capstone.Client/Pages/Medication.razor.cs
+++ July2025Capstone/July2025Capstone.Client/Pages/Medication.razor.cs
@@ -212,46 +212,4 @@ namespace July2025Capstone.Client.Pages
             Navigation.NavigateTo("/checkin-form-creation/medical-history");
         }
     }
-
-    /*
-    [Inject]
-    public AuthenticationStateProvider AuthenticationStateProvider { get; set; }
-    [Inject]
-    public HttpClient Http { get; set; }
-    public bool IsAuthenticated { get; set; } = false;
-    //public UserDTO User { get; set; } = new UserDTO();
-
-    public List<MedicationDTO> medications { get; set; }
-
-    protected override async Task OnInitializedAsync()
-    {
-        //Ensure that the user is logged in
-        var authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
-        var user = authState.User;
-
-        if(user.Identity?.IsAuthenticated == true)
-        {
-            IsAuthenticated = true;
-            try
-            {
-                //Fetch the user data (UserDTO)
-                //this.User = await Http.GetFromJsonAsync<UserDTO>("api/User");
-
-                medications = await Http.GetFromJsonAsync<List<MedicationDTO>>("https://localhost:7014/api/medications");
-            }
-            catch (Exception ex)
-            {
-
-                Console.WriteLine($"Error fetching medication data: {ex.Message}");
-                //TODO: Add better error handling
-            }
-        }
-    }
-
-    public async Task GetMovies()
-    {
-
-    }
-}
-    */
 }

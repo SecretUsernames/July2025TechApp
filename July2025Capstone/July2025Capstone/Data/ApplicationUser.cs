c470e5242afe0d072fbce89f5f143b6e5433a5d6
Vidhaan Singhvi <visi5704@colorado.edu>
2025-09-08 20:57:12 -0600

    admin panel is completed and app layour is chagned to a pretty blue

--- July2025Capstone/July2025Capstone/Data/ApplicationUser.cs
+++ July2025Capstone/July2025Capstone/Data/ApplicationUser.cs
@@ -2,9 +2,10 @@ using Microsoft.AspNetCore.Identity;
 
 namespace July2025Capstone.Data
 {
-    // Add profile data for application users by adding properties to the ApplicationUser class
+    // Extend IdentityUser with custom fields
     public class ApplicationUser : IdentityUser
     {
+        public string Status { get; set; } = "Active";  // Active, Suspended, etc.
+        public DateTime? LastLoginUtc { get; set; }     // When the user last logged in
     }
-
 }
